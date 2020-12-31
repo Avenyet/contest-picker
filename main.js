63a65aed7b4fe882c7cce6811db3189ee3f51b1f
Andrew Cashmore <andrew.cashmore@crystalthought.co.uk>
2020-12-31 13:53:32 +0000

    winners cannot be picked twice. and added some nice fireworks because why not

--- main.js
+++ main.js
@@ -1,36 +1,65 @@
+var winnerCount = 1;
+var started = false;
+var entries = [];
+
 $("#pickAWinner").click(function(){
-	$.ajax({
-		url : "entries.txt",
-		dataType: "text",
-		success : function (data) {
-			var entries = data.split("\r\n");
+	//need to use a nested element since it messed up the layout on destroy. 
+	//its probably removing styles but I don't have time to look into it
+	$("#fireworkBG .bg").fireworks('destroy');
+	
+	//fetch entries.txt if we don't have it
+	if (entries.length == 0 && started == false){
+		started = true;
+		$.ajax({
+			url : "entries.txt",
+			dataType: "text",
+			success : function (data) {
+				entries = data.split("\r\n");
+				if ( testRandomReturnsAll(entries) ){
+					getWinner(entries);
+				}else{
+					alert("ERROR: full range not possible no winner selected. Please check your entires.txt for possible incorrect data");
+				}
+			}
+		});
+	}else{
+		if (entries.length > 0){
 			if ( testRandomReturnsAll(entries) ){
 				getWinner(entries);
 			}else{
 				alert("ERROR: full range not possible no winner selected. Please check your entires.txt for possible incorrect data");
 			}
+		}else{
+			alert("All Winners have been picked!");
 		}
-	});
+	}
 });
 
+/**
+ * getWinner
+ * gets a random winner and drops the name onto the page (then trigger fireworks)
+ */
 function getWinner(entries){
-	
 	var winnerID = getRandomNumber(1,entries.length);
 	
 	//make it zero base
 	//yes I know I could have done getRandomNumber(entries.length-1) but this is cleaer
 	var realWinnerID = winnerID-1;
-	
 	var winnerName = entries[realWinnerID];
-	
 	
 	//output winner into the h1 called winnerName
 	//could write this on a single line but that looks a mess and is hard to read
 	//$("#winnerName").html(entries[getRandomNumber(entries.length-1)]);
-	$("#winnerName").html(winnerName);
-	
+	$("#winnerName").html("Winner #" +winnerCount+"<br/>"+winnerName);
+	$('#fireworkBG .bg').fireworks();
+	winnerCount++;
+	removeWinnerFromPool(realWinnerID);
 }
 
+/**
+ * getRandomNumber
+ * feteches a random number between start and end (inculsive) using rando
+ */
 function getRandomNumber(start, end){
 	return rando(start,end);
 }
@@ -46,7 +75,6 @@ function testRandomReturnsAll(entries){
 	var counter = 0;
 	
 	var expectedRange = range(1, entries.length, 1);
-	
 	while (fullRangePossible == false){
 		var newRandomNumber = getRandomNumber(1,entries.length);
 		for( var i = 0; i < expectedRange.length; i++){ 
@@ -68,11 +96,21 @@ function testRandomReturnsAll(entries){
 
 }
 
-
+/**
+ * range
+ * just generates a simple range array
+ */
 function range(start, stop, step) {
     var a = [start], b = start;
     while (b < stop) {
         a.push(b += step || 1);
     }
     return a;
+}
+/**
+ * removeWinnerFromPool
+ * remove winner so they can't be pulled again
+ */
+function removeWinnerFromPool(arrayPos){
+	entries.splice(arrayPos, 1); 
 }
